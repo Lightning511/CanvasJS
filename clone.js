2b616f2123488259c3c95938db037bac50e91d0c
caspar <caspar.lightner.25@bishops.com>
2023-01-19 15:59:06 -0800

    4th, fixed colors, initial attempt at collision.

--- clone.js
+++ clone.js
@@ -1,3 +1,6 @@
+var c = document.getElementById("mainCanvas");
+var ctx = c.getContext("2d");
+
 // class to create clone instances of bouncing rectangle
 class clone {
 
@@ -9,7 +12,45 @@ class clone {
         this.dir = dir;
         this.angle = 150;
 
+        ctx.fillStyle = "#000000";
         ctx.fillRect(this.xcoord, this.ycoord, 30, 30);
+
+    }
+
+    checkCollision()
+    {
+        if(rectEdgeL < this.x+20 <rectEdgeR || rectEdgeL < this.x < rectEdgeR)
+        {
+            if(rectEdgeT < this.y < rectEdgeB || rectEdgeT< this.y+20 < rectEdgeB)
+            {
+                cancelAnimationFrame(animate);
+                clearInterval(id);
+            }
+        }
+        if(rectEdgeT < this.y-20 <rectEdgeB || rectEdgeT < this.y < rectEdgeB)
+        {
+            if(rectEdgeL < this.x < rectEdgeR || rectEdgeL< this.x+20 < rectEdgeR)
+            {
+                cancelAnimationFrame(animate);
+                clearInterval(id);
+            }
+        }
+        if(rect2EdgeL < this.x+20 <rect2EdgeR || rect2EdgeL < this.x < rect2EdgeR)
+        {
+            if(rect2EdgeT < this.y < rect2EdgeB || rect2EdgeT< this.y+20 < rect2EdgeB)
+            {
+                cancelAnimationFrame(animate);
+                clearInterval(id);
+            }
+        }
+        if(rect2EdgeT < this.y-20 <rect2EdgeB || rect2EdgeT < this.y < rect2EdgeB)
+        {
+            if(rect2EdgeL < this.x < rect2EdgeR || rect2EdgeL< this.x+20 < rect2EdgeR)
+            {
+                cancelAnimationFrame(animate);
+                clearInterval(id);
+            }
+        }
     }
 
     
@@ -17,44 +58,48 @@ class clone {
         const id = setInterval(() =>
         {
             // clear the canvas to draw new rectangle (creates movement illusion)
-            ctx.clearRect(this.x,this.y, 30, 30);
+            // ctx.clearRect(this.x,this.y, 30, 30);
             if (this.dir == "right"){
                 var angleRad = this.angle * (Math.PI/180);
                 this.x+= this.speed * Math.cos(angleRad);
                 this.y-= this.speed * Math.sin(angleRad);
-            ctx.fillRect(this.x,this.y,20,20);
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(this.x,this.y,20,20);
             }
             if (this.dir == "left") {
                 var angleRad = this.angle * (Math.PI/180);
                 this.x-= this.speed * Math.cos(angleRad);
                 this.y+= this.speed * Math.sin(angleRad);
-            ctx.fillRect(this.x,this.y,20,20);
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(this.x,this.y,20,20);
             }
             if (this.dir == "up") {
                 var angleRad = this.angle * (Math.PI/180);
                 this.x-= this.speed * Math.cos(angleRad);
                 this.y-= this.speed * Math.sin(angleRad);
-            ctx.fillRect(this.x,this.y,20,20);
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(this.x,this.y,20,20);
             }
             if (this.dir == "down") {
                 var angleRad = this.angle * (Math.PI/180);
                 this.x+= this.speed * Math.cos(angleRad);
                 this.y+= this.speed * Math.sin(angleRad);
-            ctx.fillRect(this.x,this.y,20,20);
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(this.x,this.y,20,20);
             }
-            if (this.xcoord >= c.width) {
+            if (this.x >= c.width) {
                 this.dir = "left";
                 this.angle = 30 * Math.random();
             }
-            if (this.xcoord <= 0) {
+            if (this.x <= 0) {
                 this.dir = "right";
                 this.angle = 30 * Math.random();
             }
-            if (this.ycoord <= 0) {
+            if (this.y <= 0) {
                 this.dir = "down";
                 this.angle = 30 * Math.random();
             }
-            if (this.ycoord >= c.height) {
+            if (this.y >= c.height) {
                 this.dir = "up";
                 this.angle = 30 * Math.random();
             }
--- newGame.js
+++ newGame.js
@@ -2,26 +2,34 @@ var c = document.getElementById("mainCanvas");
 var ctx = c.getContext("2d");
 
 // declares variables and values
-var moveX = 200;
-var moveY = 50;
+var moveX = 900;
+var moveY = 280;
 
-var moveX2 = 50;
-var moveY2 = 50;
+var moveX2 = 300;
+var moveY2 = 280;
 
 var rectEdgeL;
 var rectEdgeT;
+var rectEdgeR;
+var rectEdgeB;
 
 var rect2EdgeL;
 var rect2EdgeT;
+var rect2EdgeR;
+var rect2EdgeB;
+
 
 var dir = "right";
+// array for the speed and dir to randomize clone movement
 var dirs = ["right", "left", "up", "down"];
-var speeds = [5, 10, 20, 30]
+var speeds = [5, 10, 15, 20];
+
 
 
 // attracts the attention of the computer, to notify if key is pressed
 window.addEventListener("keydown", keysPressed);
 //window.addEventListener("keyup", keysPressed);
+window.addEventListener("keydown", restartGame);
 
 
 
@@ -49,33 +57,102 @@ function keysPressed(e)
   e.preventDefault();
 
 }
-
-function drawRectangle(x, y, width, height)
+//calls do stuff again if r is pressed, essentially restarts the game
+function restartGame(e)
 {
-  ctx.fillRect(x,y,width,height);
-
-  var r = Math.floor(Math.random() * 255);
-  var g = Math.floor(Math.random() * 255);
-  var b = Math.floor(Math.random() * 255);
-  ctx.fillStyle = "rgba(r, g, b, .5)";
-  ctx.fill();
+  var keys = e.keyCode;
+  if(keys == 82)
+  {
+    doStuff();
+  }
 }
-
+// draws the first movable square to be controlled by user
+function drawMover1(rectEdgeL, rectEdgeT)
+{
+  this.ctx.fillStyle = "#0000FF";
+  ctx.fillRect(rectEdgeL, rectEdgeT, 40, 40);
+  ctx.strokeStyle = "#000000";
+  ctx.strokeRect(rectEdgeL, rectEdgeT, 40, 40);
+  console.log("red");
+}
+// draws the second movable square to be controlled by user
+function drawMover2(rect2EdgeL, rect2EdgeT)
+{
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(rect2EdgeL, rect2EdgeT, 40, 40);
+  ctx.strokeStyle = "#000000";
+  ctx.strokeRect(rect2EdgeL, rect2EdgeT, 40, 40);
+  console.log("blue");
+}
+// draws bouncing rectangle that creates clones as it hits the walls of the canvas
+function drawBouncingRect(x, y)
+{
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(x, y, 20, 20);
+  console.log("black");
+}
+// animates the entire canvas and code 
 function animate()
 {
-  ctx.clearRect(rectEdgeL, rectEdgeT, 40, 40);
-  ctx.clearRect(rect2EdgeL, rect2EdgeT, 40, 40);
+  //ctx.clearRect(rectEdgeL, rectEdgeT, 40, 40);
+  //ctx.clearRect(rect2EdgeL, rect2EdgeT, 40, 40);
   
   rectEdgeL = moveX - 20;
   rectEdgeT = moveY - 20;
   rect2EdgeL = moveX2 - 20;
   rect2EdgeT = moveY2 - 20;
 
-  drawRectangle(rectEdgeL, rectEdgeT, 40, 40);
-  drawRectangle(rect2EdgeL, rect2EdgeT, 40, 40);
+  //ctx.fillStyle = color1;
+  //ctx.fillRect(rectEdgeL, rectEdgeT, 40, 40);
+  drawMover1(rectEdgeL, rectEdgeT);
+  //ctx.fillStyle = color2;
+  //ctx.fillRect(rect2EdgeL, rect2EdgeT, 40, 40);
+  drawMover2(rect2EdgeL, rect2EdgeT);
 
   requestAnimationFrame(animate);
   console.log("requestAnimationFrame called");
+}
+
+function checkCollision(x, y)
+{
+  rectEdgeR = rectEdgeL + 40;
+  rectEdgeB = rectEdgeT - 40;
+  rect2EdgeR = rect2EdgeL + 40;
+  rect2EdgeB = rect2EdgeT - 40;
+
+  if(rectEdgeL < x+20 <rectEdgeR || rectEdgeL < x < rectEdgeR)
+  {
+    if(rectEdgeT < y < rectEdgeB || rectEdgeT< y+20 < rectEdgeB)
+    {
+      cancelAnimationFrame(animate);
+      //clearInterval(period);
+    }
+  }
+  if(rectEdgeT < y-20 <rectEdgeB || rectEdgeT < y < rectEdgeB)
+  {
+    if(rectEdgeL < x < rectEdgeR || rectEdgeL< x+20 < rectEdgeR)
+    {
+      cancelAnimationFrame(animate);
+      //clearInterval(period);
+    }
+  }
+  if(rect2EdgeL < x+20 <rect2EdgeR || rect2EdgeL < x < rect2EdgeR)
+  {
+    if(rect2EdgeT < y < rect2EdgeB || rect2EdgeT< y+20 < rect2EdgeB)
+    {
+      cancelAnimationFrame(animate);
+      //clearInterval(period);
+    }
+  }
+  if(rect2EdgeT < y-20 <rect2EdgeB || rect2EdgeT < y < rect2EdgeB)
+  {
+    if(rect2EdgeL < x < rect2EdgeR || rect2EdgeL< x+20 < rect2EdgeR)
+    {
+      cancelAnimationFrame(animate);
+      //clearInterval(period);
+    }
+  }
+
 }
 
 function bounceRect()
@@ -84,7 +161,7 @@ function bounceRect()
   var y = 100;
   var speed = 25;
   var angle = 150;
-  drawRectangle(x,y,20,20);
+  drawBouncingRect(x,y);
   const id = setInterval(() =>
   {
     // clear the canvas to draw new rectangle (creates movement illusion)
@@ -93,49 +170,65 @@ function bounceRect()
       var angleRad = angle * (Math.PI/180);
       x+= speed * Math.cos(angleRad);
       y-= speed * Math.sin(angleRad);
-      drawRectangle(x,y,20,20);
+      drawBouncingRect(x,y);
+      checkCollision(x,y);
     }
     if (dir == "left") {
       var angleRad = angle * (Math.PI/180);
       x-= speed * Math.cos(angleRad);
       y+= speed * Math.sin(angleRad);
-      drawRectangle(x,y,20,20);
+      drawBouncingRect(x,y);
+      checkCollision(x,y);
     }
     if (dir == "up") {
       var angleRad = angle * (Math.PI/180);
       x-= speed * Math.cos(angleRad);
       y-= speed * Math.sin(angleRad);
-      drawRectangle(x,y,20,20);
+      drawBouncingRect(x,y);
+      checkCollision(x,y);
     }
     if (dir == "down") {
       var angleRad = angle * (Math.PI/180);
       x+= speed * Math.cos(angleRad);
       y+= speed * Math.sin(angleRad);
-      drawRectangle(x,y,20,20);
+      drawBouncingRect(x,y);
+      checkCollision(x,y);
     }
-    if (x >= c.width) {
+    if (x >= c.width-10) {
       dir = "left";
       angle = 30 * Math.random();
-      var C = new clone(x,y,speeds[Math.random()*4], dirs[Math.random()*4]);
+      var cloneDir = dirs[0];
+      var cloneSpeed = speeds[Math.floor(Math.random() * 4)]
+      var C = new clone(x,y, cloneSpeed, cloneDir);
       C.bounceClone();
+      C.checkCollision();
     }
-    if (x <= 0) {
+    if (x <= 10) {
       dir = "right";
       angle = 30 * Math.random();
-      var C = new clone(x,y,speeds[Math.random()*4], dirs[Math.random()*4]);
+      var cloneDir = dirs[1];
+      var cloneSpeed = speeds[Math.floor(Math.random() * 4)]
+      var C = new clone(x,y, cloneSpeed, cloneDir);
       C.bounceClone();
+      C.checkCollision();
     }
-    if (y <= 0) {
+    if (y <= 10) {
       dir = "down";
       angle = 30 * Math.random();
-      var C = new clone(x,y,speeds[Math.random()*4], dirs[Math.random()*4]);
+      var cloneDir = dirs[2];
+      var cloneSpeed = speeds[Math.floor(Math.random() * 4)]
+      var C = new clone(x,y, cloneSpeed, cloneDir);
       C.bounceClone();
+      C.checkCollision();
     }
-    if (y >= c.height) {
+    if (y >= c.height-10) {
       dir = "up";
       angle = 30 * Math.random();
-      var C = new clone(x,y,speeds[Math.random()*4], dirs[Math.random()*4]);
+      var cloneDir = dirs[3];
+      var cloneSpeed = speeds[Math.floor(Math.random() * 4)]
+      var C = new clone(x,y, cloneSpeed, cloneDir);
       C.bounceClone();
+      C.checkCollision();
   }
 
   }, 75);
